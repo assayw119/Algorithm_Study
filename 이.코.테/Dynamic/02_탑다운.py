8b0646e85a339b8b2980787ab6803bf46cc7f746
YoungWoong Park <assayw119@naver.com>
2022-04-17 18:53:36 +0900

    Dynamic Programming

---
+++ 이.코.테/Dynamic/02_탑다운.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+d = [0] * (n+1)
+
+def fibo(x):
+    # 종료 조건 1 (1 혹은 2일때 1 반환)
+    if x == 1 or x == 2:
+        return 1
+    # 이미 계산한 적 있는 문제라면 그대로 반환
+    if d[x] != 0:
+        return d[x]
+    # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+print(fibo(n))
