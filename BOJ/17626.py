104269181aca22765da394d375beffa8f97a5144
YoungWoong Park <assayw119@naver.com>
2022-04-18 23:43:39 +0900

    Update 17626.py

--- BOJ/17626.py
+++ BOJ/17626.py
@@ -2,15 +2,31 @@ import math
 from itertools import combinations
 from itertools import product
 
-# n = int(input())
-# num_list = [True for i in range(int(math.sqrt(n)))]
-# sqrt_list = [i*i for i in range(int(math.sqrt(n)),0,-1)]
+n = int(input())
+num_list = [True for i in range(int(math.sqrt(n)))]
+sqrt_list = [i*i for i in range(int(math.sqrt(n)),0,-1)]
 
-def test(a):
-    for i in range(a):
-        if a-i in sqrt_list:
-            test(i)
-print(test(n))
+# def test(a):
+#     s = int(math.sqrt(a))
+#     sqrt_list = [i * i for i in range(s, 0, -1)]
+#
+#     for i in range(a,0,-1):
+#         # if i in sqrt_list:
+#         #     new_a = a - i
+#         #     test(new_a)
+#         #     return new_a
+#         count = 0
+#         count_lst = []
+#         for j in range(0,a+1):
+#             if a-j in sqrt_list:
+#                 new_a = a - (i - j)
+#                 print(new_a)
+#                 count += 1
+#                 test(new_a)
+#             count_lst.append(count)
+#             return count_lst
+# print(test(n))
+
 
 dp = [int(1e9)] * (n+1)
 
