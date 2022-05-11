51e2ed3bd01fc4c2376f49a1359a2aa29d371fb8
YoungWoong Park <assayw119@naver.com>
2022-05-11 23:55:08 +0900

    Update 22857.py

--- BOJ/22857.py
+++ BOJ/22857.py
@@ -1,16 +1,22 @@
 n, k = map(int,input().split())
 s = list(map(int, input().split()))
 
+def max_1(lst):
 
-count = 0
-for i in s:
-    if i % 2 != 0:
-        count += 1
-        # print(i)
-        s.remove(i)
-    if k == count:
-        break
-    elif k > count:
+    max_count = 0
+    count = 0
+    for i in range(len(lst)-1):
+        if lst[i] == 1 and lst[i] == lst[i+1]:
+            count += 1
+        else:
+            
 
-for i in s:
-    if i % 2 == 0:
+
+
+for i in range(len(s)):
+    if s[i] % 2 == 1:
+        s[i] = 0
+    else:
+        s[i] = 1
+
+print(max_1(s))
