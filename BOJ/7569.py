b94ea39247da029e1cb53fa6a68d6b713d7b054a
YoungWoong Park <assayw119@naver.com>
2022-05-26 20:48:50 +0900

    Create 7569.py

---
+++ BOJ/7569.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+m,n,h = map(int, input().split())
+arr = []
+for i in range(n):
+    t = list(map(int, input().split()))
+    arr.append(t)
+
+q = deque(arr)
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+dz = 
+
+while q:
