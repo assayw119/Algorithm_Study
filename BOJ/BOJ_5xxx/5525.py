dd7673c0434726e938439caca0a92c4806808af8
assayw119 <assayw119@naver.com>
2022-07-24 20:28:28 +0900

    220724 BOJ IOIOI sol.

--- BOJ/BOJ_5xxx/5525.py
+++ BOJ/BOJ_5xxx/5525.py
@@ -1,16 +1,39 @@
-n = int(input())
-m = int(input())
-s = list(input())
+# n = int(input())
+# m = int(input())
+# s = list(input())
+#
+# k = 2*n+1
+# result = 0
+# def check(x):
+#     global s, n, result, k
+#
+#     # print(s[x:x+k], x)
+#     if s[x:x+k] == ['I'] + ['O', 'I']*n:
+#         result += 1
+#
+# for i in range(m-k):
+#     check(i)
+# print(result)
+
+import sys
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+s = sys.stdin.readline().rstrip()
 
 k = 2*n+1
 result = 0
-def check(x):
-    global s, n, result, k
-
-    # print(s[x:x+k], x)
-    if s[x:x+k] == ['I'] + ['O', 'I']*n:
-        result += 1
-
-for i in range(m-k):
-    check(i)
+num = 0
+ioi_count = 0
+while 1:
+    if s[num:num+3] == 'IOI':
+        num += 2
+        ioi_count += 1
+        if ioi_count == n:
+            result += 1
+            ioi_count -= 1
+    else:
+        num += 1
+        ioi_count = 0
+    if num >= m-1:
+        break
 print(result)
