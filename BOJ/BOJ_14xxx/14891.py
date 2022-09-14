8aa3a183c29241ee662ebebc746f176ecc9cb880
assayw119 <assayw119@naver.com>
2022-09-15 01:02:06 +0900

    Update 14891.py

--- BOJ/BOJ_14xxx/14891.py
+++ BOJ/BOJ_14xxx/14891.py
@@ -1,26 +1,59 @@
-topni = [list(input()) for _ in range(4)]
+import sys
+input = sys.stdin.readline
 
+topni = [list(input()) for _ in range(4)]
+# 0 = N극!!
+# 1 = S극!!
 k = int(input())
-turn = [list(map(int, input().split())) for _ in range(k)]
+# turn = [list(map(int, input().split())) for _ in range(k)]
 
 def rotate(lst, dir):
-    if dir == 1:
+    if dir == 1: # 시계방향 회전
         lst.insert(0, lst[-1])
-    else:
+        del lst[-1]
+    elif dir == -1: # 반시계방향 회전
         a = lst[0]
         del lst[0]
         lst.append(a)
+    else:
+        pass
     return lst
-for i in range(k):
+for _ in range(k):
 
-    num, direction = turn[i][0], turn[i][1]
+    num, direction = map(int, input().split())
 
-    left_sn = topni[num][6]
-    right_sn = topni[num][2]
+    left_sn = topni[num-1][6]
+    right_sn = topni[num-1][2]
 
-    # 회전
-    topni[num] = rotate(topni[num], direction)
-    
+    rotate_how = {}
+    rotate_how[num-1] = direction
+
+    for i in range(1,4):
+        if num + i <= k: # 오른쪽에 있는 톱니바퀴들
+            left_sn_next2 = topni[num+i-1][6]
+            right_sn_next1 = topni[num+i-2][2]
+
+            if right_sn_next1 == left_sn_next2:
+                rotate_how[num+i-1] = 0
+            else:
+                rotate_how[num+1-i] = -direction
+
+        if num - i > 0:
+            right_sn_next2 = topni[num-i-2][6]
+            left_sn_next1 = topni[num-i-1][2]
+
+            if right_sn_next2 == left_sn_next1:
+                rotate_how[num-i-1] = 0
+            else:
+                rotate_how[num-i-1] = -direction
+
+    for k in range(4):
+
+        # 회전
+        # topni[num-1] = rotate(topni[num-1], direction)
+        topni[k] = rotate(topni[k], rotate_how[k])
+        print(topni[k])
+
 
 
 
